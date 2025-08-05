5e82017b90d4f111813db5484506daf5fae72571
Waleed-SoftwareDev <moha1193@algonquinlive.com>
2025-08-05 13:08:15 -0400

    Create StringComparison.py

---
+++ StringComparison.py
@@ -0,0 +1,17 @@
+# Simple User prompt checker
+# This program checks if two strings are equal or not using == and != operators
+
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+
+# Using == operator
+if string1 == string2:
+    print("Both strings are equal.")
+else:
+    print("Both strings are NOT equal.")
+
+# Using != operator
+if string1 != string2:
+    print("The strings are different.")
+else:
+    print("The strings are the same.")
