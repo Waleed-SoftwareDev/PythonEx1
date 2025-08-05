62d4526829c0ca67bb155212e81b2be7a1375a43
Waleed-SoftwareDev <moha1193@algonquinlive.com>
2025-08-05 12:57:04 -0400

    Create DefiningAndCallingAFunction.py

---
+++ DefiningAndCallingAFunction.py
@@ -0,0 +1,11 @@
+#Lab 4
+#Written by Waleed Mohamed
+
+def intro(name):
+    #Define Function
+
+    print(f"Hello my name is {name} and I like python!")
+    #Print Introduction
+
+intro("waleed")
+#Call function
